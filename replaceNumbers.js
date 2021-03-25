52cbdfedf5f476ea3eb4d82b6a14c5d21642b496
Kalina <kalina984@abv.bg>
2021-03-25 11:11:58 +0200

    added new HW

--- replaceNumbers.js
+++ replaceNumbers.js
@@ -1,3 +1,9 @@
 // Given is the string: 'a1b2c3d'
 // Write a program which will replace each digit in string with '-'. I.e. the resulting string should be 'a-b-c-d'
 
+let string = 'a1b2c3d';
+
+const re = /\d/g;
+
+let newString = string.replace(re, '-');
+console.log(`New string is: ${newString}`); 
